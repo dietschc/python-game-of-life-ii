37c2591a03067bca9089f8fc7f353b97a95f5152
Coleman Dietsch <dietschc@csp.edu>
2024-03-21 16:11:22 -0500

    Check neighbors sort of halfway working

--- python-game-of-life-ii/grid.py
+++ python-game-of-life-ii/grid.py
@@ -3,41 +3,69 @@ from numpy import random
 grid = []
 rows = 10
 cols = 10
-rrand = random.randint(10)
-crand = random.randint(10)
+rrand = random.randint(rows)
+crand = random.randint(cols)
 
-# Grid setup code
 # Generate grid and fill with zeroes
-for x in range(rows):
-    col = []
-    for y in range(cols):
-        col.append(0)
-    grid.append(col)
+def setup_grid():
+    for x in range(rows):
+        col = []
+        for y in range(cols):
+            col.append(0)
+        grid.append(col)
 
-# Print random initial condition
-print("row rand: " + str( rrand) )
-print("col rand: " + str( crand) )
+    # Print random initial condition
+    print("row rand: " + str( rrand) )
+    print("col rand: " + str( crand) )
 
-# Set initial random seed position
-grid[rrand][crand] = 1
+    # Set initial random seed position
+    grid[rrand][crand] = 1
 
 # Function to print entire grid matrix to the display
 def output_grid():
     # Print whole matrix
     for x in grid:
         print(x)
-    print() # print blank line
+    print() # blank line
 
+# Look for living cells
 def iterate_grid():
+    row_current = ''
+    col_current = ''
+
     for x in grid:
         for y in x:
             if y == 1:
-                print("row: " + str( grid.index(x) ))
-                print("column: " + str( x.index(y) ))
+                row_current = grid.index(x)
+                col_current = x.index(y)
+
+                # Debug currently selected row and column
+                #print("row: " + str( row_current ))
+                #print("column: " + str( col_current ))
+
+                check_neighbors(row_current, col_current)
+
+# Iterate through neighboring cells
+def check_neighbors(row, col):
+    print("this row: " + str( row ))
+    print("this col: " + str( col ))
+
+    # Should always be true if calling function
+    if grid[row][col]:
+        print("alive")
+
+    # Trick to read the rows and cols around current cell
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            print("row: " + str( row + x ))
+            print("col: " + str( col + y ))
+            # Need to implement code to 'wrap-around' when neighbors are below 0 or above 9
+
 
 
 # Main
 if __name__=="__main__":
+    setup_grid()
     output_grid()
     iterate_grid()
 
